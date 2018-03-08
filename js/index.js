91530fbdf2acc2e8098ef73bc4247167c490bc3c
qiaofan <fan.qiao@51auto.com>
2018-03-08 14:43:00 +0800

    完善增删改查功能
    完善增删改查功能

--- js/index.js
+++ js/index.js
@@ -2,8 +2,8 @@ var myapp=angular.module('myapp',[]);
 	myapp.controller('rdCtrl', ['$scope', function($scope){
 		var d=localStorage.data;
 		$scope.liebiao=d?JSON.parse(d):[];
+		console.log($scope.liebiao)
 		var del=localStorage.delete;
-		$scope.deleteject=[];
 		$scope.color=["red","yellow","yellowgreen","pink","blue","orange"];
 		$scope.cindex=0;
 		$scope.indexTime=function(index){
@@ -14,12 +14,29 @@ var myapp=angular.module('myapp',[]);
 			var data={
 				name:"新列表"+ ($scope.liebiao.length+1),
 				color:$scope.color[$scope.liebiao.length%6],
-				items:[]
+				items:[],
+				completeject:[]
 			}
 			$scope.liebiao.push(data);
 			localStorage.data=JSON.stringify($scope.liebiao);
 		}
-		//console.log($scope.liebiao[$scope.cindex].items)
+		/*删除列表*/
+		$scope.deleteList=function(index){
+			var cu=$scope.liebiao;
+			var r=[];
+			console.log(cu);
+			for(var i=0;i<cu.length;i++){
+				if(i != index){
+					r.push(cu[i]);
+				}
+			}
+			$scope.liebiao=r;
+			localStorage.data=JSON.stringify($scope.liebiao)
+		}
+		//保存名称
+		$scope.storageName=function(){
+			localStorage.data=JSON.stringify($scope.liebiao);
+		}
 		/*点击增加项目*/
 		$scope.addproject=function(){
 			var cu=$scope.liebiao[$scope.cindex];
@@ -27,7 +44,7 @@ var myapp=angular.module('myapp',[]);
 				name:"新项目",
 				color:cu.color+'-nav'
 			};
-			//console.log($scope.liebiao[$scope.cindex].items)
+			console.log(typeof lists)
 			$scope.liebiao[$scope.cindex].items.push(lists);
 			localStorage.data=JSON.stringify($scope.liebiao);
 		}
@@ -40,12 +57,39 @@ var myapp=angular.module('myapp',[]);
 					r.push(cu.items[i])
 				}
 			}
-			$scope.liebiao[$scope.cindex].items = r; 
+			$scope.liebiao[$scope.cindex].items = r;
    			localStorage.data = JSON.stringify($scope.liebiao);
 		}
 		/*完成项目*/
 		$scope.Projectreach=function(index){
 			var r=$scope.liebiao[$scope.cindex].items.splice(index,1);
-			$scope.deleteject.push(r);
+			$scope.liebiao[$scope.cindex].completeject.push(r[0]);
+			localStorage.data=JSON.stringify($scope.liebiao);
 		} 
+		/*点击删除已完成项*/
+		$scope.deleteOpen=function(index){
+			var cu=$scope.liebiao[$scope.cindex];
+			var r=[];
+			for(var i=0;i<cu.completeject.length;i++){
+				if(i != index){
+					r.push(cu.completeject[i])
+				}
+			}
+			$scope.liebiao[$scope.cindex].completeject = r;
+   			localStorage.data = JSON.stringify($scope.liebiao);
+		}
+		/*查看完成项目*/
+		$scope.openReach=function(){
+			var reach = $(".reach").attr("value");
+			if(reach == "none"){
+				$(".reach").attr("value","block");
+				$(".reach").css("display","block");
+			}
+			if(reach == "block"){
+				$(".reach").attr("value","none");
+				$(".reach").css("display","none");
+			}
+			
+		}
+
 	}])
